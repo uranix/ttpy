b011ccf5c4ce5f93c7b02f938385432325012569
Ivan Tsybulin <tsybulinhome@gmail.com>
2016-09-03 15:28:50 +0300

    Revert "Import order changed to break tools dependency"
    This reverts commit 3a75fd530b1ecb9e6466ac99532d06032ae3a049.

--- tt/core/tt.py
+++ tt/core/tt.py
@@ -1,13 +1,19 @@
 # Here we import all necessary staff from external files
 
+# main classes
+from .matrix import matrix
+from .vector import vector, tensor
+
+
 # tools
 from .tools import matvec, col, kron, dot, mkron, concatenate, sum, reshape
 from .tools import eye, diag, Toeplitz, qshift, qlaplace_dd, IpaS
 from .tools import ones, rand, linspace, sin, cos, delta, stepfun, unit, xfun
 
-# main classes
-from .matrix import matrix
-from .vector import vector, tensor
-
 # utility
 from . import utils
+
+
+
+
+
